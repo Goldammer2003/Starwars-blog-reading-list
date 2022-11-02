d5b793b81d8bb52ef64d846baa8861ecb65c64c9
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-11-02 00:11:09 +0000

    Alternative approach

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,28 +1,24 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: []
 		},
 		actions: {
+
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				
+					fetch ("https://swapi.dev/api/people", {
+						method:"GET", 
+						headers: {"Content-Type":"application/json"}
+					})
+					.then (response =>response.json())
+					.then (data =>setStore({people:data}) )
+					
+				
 			},
 			changeColor: (index, color) => {
 				//get the store
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,27 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React, {useState,useContext, useEffect} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Card from "../component/card";
-
+import {Context} from "../store/appContext"
 
 export const Home = () => {
-const [characters,setCharacters] =useState ([])
-useEffect (()=> {
-	getCharacters()
-}, []) 
-	const getCharacters = () => {
-		fetch ("https://swapi.dev/api/people")
-		.then (response =>response.json())
-		.then (data =>setCharacters (data.results) )
-		
-	}
-	console.log (characters)
+	const {store, actions} = useContext(Context)
+	console.log("hello",store)
+
+	
 	return (
-		<div className="text-center mt-5">
-		{characters.map ((item)=>{
-			return (
-				<Card obj={item}/>
-			)
-		})}
+		<div className="text-center mt-5 row container">
+			 <div className="col-md-8">
+      			
+		
+			
+			 {store.people.results && store.people.results.map((item,index)=>{
+
+          return (
+			<div className="card-group">
+ <div className="card" style={{ width : '18rem' }}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <p className="card-text">height: {item.height} </p>
+              <p className ="card-text">Hair-color: {item.hair_color}</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+			</div>
+           
+                
+          )
+        })}
+			
+
 	</div>
+	</div> 
+	
 	)}
