6535ee09193f3c5b9978386569a870aba6741ae1
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-11-16 17:43:33 +0000

    Fix in the detailsVehicles.js file

--- src/js/views/detailsVehicles.js
+++ src/js/views/detailsVehicles.js
@@ -8,7 +8,7 @@ export const IndividualpageinformationforVehicles = () => {
     const {store}=useContext (Context)
     const params = useParams();
     console.log("hey",store.characters?.results[params.theid])
-    const InformationforVehiclesinLearnMore = store.characters?.results[params.theid]
+    const InformationforVehiclesinLearnMore = store.vehicles?.results[params.theid]
     return <div className="container">
         <div className="d-flex border-bottom border-danger pb-5"> <img className="individualpagepictures"src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"></img>
       <div> <h1>{InformationforCharactersinLearnMore.name}</h1> 
